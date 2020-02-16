1d87dfa8d632e077a9c8a85f72c5f24c8c97086f
tormobr <tormod.brandshoi@gmail.com>
2020-02-16 18:54:31 +0100

    added some animations

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,13 @@
+import time
+from copy import deepcopy
 import random
-from matplotlib import pyplot as plt
 import sorting_lib as sl
 
 class Bubble:
     def __init__(self, n):
         self.n = n
         self.arr = sl.gen_list(self.n)
+        self.arrays = []
 
     def __repr__(self):
         return sl.print_arr(self.arr)
@@ -14,12 +16,17 @@ class Bubble:
         for i in range(self.n):
             for j in range(self.n - 1):
                 if self.arr[j] > self.arr[j+1]:
-                    sl.swap(arr, j, j+1)
+                    sl.swap(self.arr, j, j+1)
+                self.arrays.append(deepcopy(self.arr))
 
 
 if __name__ == "__main__":
-    b = Bubble(10)
+    b = Bubble(20)
     print(b)
+    s = time.time()
     b.sort()
+    print(f"Runtime: {time.time()-s}")
     print(b)
-    print(f"is_sorted = {b.check()}")
+    print(f"is_sorted = {sl.check(b.arr, b.n)}")
+    sl.animate(b.arr, b.arrays)
+
---
+++ sorting_lib.py
@@ -0,0 +1,45 @@
+import random
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import numpy as np
+
+def print_arr(arr):
+    return "".join(str(x) + ", " for x in arr)
+
+def swap(arr, i, j):
+    tmp = arr[i] 
+    arr[i] = arr[j]
+    arr[j] = tmp
+
+def gen_list(n):
+    ret = [i+1 for i in range(n)]
+    random.shuffle(ret)
+    return ret
+
+def check(arr, n):
+    for i in range(n - 1):
+        if arr[i+1] < arr[i]:
+            return False
+    return True
+
+def ani_iteration(i, rects, data):
+    for rect, height in zip(rects, data[i]):
+        if check(data[i], len(data[i])):
+            rect.set_color("lightgreen")
+        else:
+            rect.set_color("white")
+        rect.set_height(height)
+    return rects
+
+def animate(arr, data):
+    fig = plt.figure()
+
+    x = arr
+    plt.style.use("dark_background")
+    rects = plt.bar(x, data[0], color="w")
+    plt.ylim(0, max(arr)+10)
+
+    anim = animation.FuncAnimation(fig, ani_iteration, fargs=(rects, data), frames=len(data), interval=1)
+    plt.show()
+    #anim.save("plot.mp4")
+
