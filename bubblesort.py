8872abb9ee9e6373103a34d99d72dbbc6e1fe0fc
tormobr <tormod.brandshoi@gmail.com>
2020-02-16 17:21:08 +0100

    bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,17 +1,38 @@
+import random
 from matplotlib import pyplot as plt
 
 class Bubble:
     def __init__(self, n):
         self.n = n
-        self.arr = get_list(self.n)
+        self.arr = self.gen_list()
     
     def __repr__(self):
-        return "".join(self.arr)
+        return "".join(str(x) + ", " for x in self.arr)
 
-    
+    def sort(self):
+        for i in range(self.n):
+            for j in range(self.n - 1):
+                if self.arr[j] > self.arr[j+1]:
+                    self.swap(j, j+1)
 
-    def gen_list(self, n):
-        return [random.randrange(i) for i in range(n)]
+    def swap(self, i, j):
+        tmp = self.arr[i] 
+        self.arr[i] = self.arr[j]
+        self.arr[j] = tmp
+
+    def gen_list(self):
+        return [random.randrange(self.n) for i in range(self.n)]
+
+    def check(self):
+        for i in range(self.n - 1):
+            if self.arr[i+1] < self.arr[i]:
+                return False
+        return True
+
 
 if __name__ == "__main__":
-    pass
+    b = Bubble(10)
+    print(b)
+    b.sort()
+    print(b)
+    print(f"is_sorted = {b.check()}")
