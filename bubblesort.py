6c2b98aa399c8d25672d60cf3053d9ffbf32f97e
tormobr <tormod.brandshoi@gmail.com>
2020-02-16 17:30:25 +0100

    bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,33 +1,20 @@
 import random
 from matplotlib import pyplot as plt
+import sorting_lib as sl
 
 class Bubble:
     def __init__(self, n):
         self.n = n
-        self.arr = self.gen_list()
-    
-    def __repr__(self):
-        return "".join(str(x) + ", " for x in self.arr)
+        self.arr = sl.gen_list(self.n)
 
+    def __repr__(self):
+        return sl.print_arr(self.arr)
+    
     def sort(self):
         for i in range(self.n):
             for j in range(self.n - 1):
                 if self.arr[j] > self.arr[j+1]:
-                    self.swap(j, j+1)
-
-    def swap(self, i, j):
-        tmp = self.arr[i] 
-        self.arr[i] = self.arr[j]
-        self.arr[j] = tmp
-
-    def gen_list(self):
-        return [random.randrange(self.n) for i in range(self.n)]
-
-    def check(self):
-        for i in range(self.n - 1):
-            if self.arr[i+1] < self.arr[i]:
-                return False
-        return True
+                    sl.swap(arr, j, j+1)
 
 
 if __name__ == "__main__":
